f8e21eff81fac992685befc88c669d4771ae88da
Diego Carnero <diego.carnero.ortega@gmail.com>
2022-12-12 10:50:19 +0100

    Initial commit

---
+++ android/src/com/mygdx/proyectocoches/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.proyectocoches;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.proyectocoches.ProyectoCOCHES;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProyectoCOCHES(), config);
+	}
+}
