d3d25cb0732a0da31a1c2432cbdac0fe74868079
Diego Carnero <diego.carnero.ortega@gmail.com>
2023-01-11 13:02:48 +0100

    Creada MiOrthoCam para ajustar el zoom dependiendo de la velocidad

--- core/src/com/mygdx/proyectocoches/screens/TestDrive.java
+++ core/src/com/mygdx/proyectocoches/screens/TestDrive.java
@@ -5,7 +5,6 @@ import static com.mygdx.proyectocoches.Constantes.PPM;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -17,6 +16,7 @@ import com.mygdx.proyectocoches.formas.Coche;
 import com.mygdx.proyectocoches.formas.GeneradorCircuito;
 import com.mygdx.proyectocoches.ui.TestOsd;
 import com.mygdx.proyectocoches.utils.InputManager;
+import com.mygdx.proyectocoches.utils.MiOrthoCam;
 
 import java.util.ArrayList;
 
@@ -25,7 +25,7 @@ public class TestDrive implements Screen {
     private final SpriteBatch miBatch;
     private final World miWorld;
     private final Box2DDebugRenderer miB2dr;
-    private final OrthographicCamera miCam;
+    private final MiOrthoCam miCam;
     private final Viewport miViewport;
     private final Body jugador;
     private final ArrayList<Body> circuito;
@@ -39,8 +39,7 @@ public class TestDrive implements Screen {
         this.miBatch = new SpriteBatch();
         this.miWorld = new World(new Vector2(0,0),true);
         this.miB2dr = new Box2DDebugRenderer();
-        this.miCam = new OrthographicCamera();
-        miCam.zoom = 1.5F;
+        this.miCam = new MiOrthoCam();
         this.miViewport = new FitViewport(1440F/PPM,720F/PPM,miCam);
 
         this.circuito = GeneradorCircuito.Cargar(miWorld,"worlds/test_loop.tmx");
@@ -66,6 +65,7 @@ public class TestDrive implements Screen {
 
     private void update(float delta) {
         miCam.position.set(jugador.getPosition(),0);
+        this.miCam.AdjustaZoomPorVelo(jugador);
         miCam.update();
         miWorld.step(delta,6,2);
     }
---
+++ core/src/com/mygdx/proyectocoches/utils/MiOrthoCam.java
@@ -0,0 +1,44 @@
+package com.mygdx.proyectocoches.utils;
+
+import static com.mygdx.proyectocoches.Constantes.MAX_VELOCIDAD_FORW;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.physics.box2d.Body;
+
+public class MiOrthoCam extends OrthographicCamera {
+
+    private static final float ZOOM_BAJO = 0.4f;
+    private static final float ZOOM_ALTO = 0.8f;
+    private static final float VELO_LIM_BAJO = 4f;
+    private static final float VELO_LIM_ALTO = 6f;
+
+    private float ultimaVelo;
+
+    public MiOrthoCam() {
+        super();
+        this.zoom = ZOOM_BAJO;
+    }
+
+    public void AdjustaZoomPorVelo(Body jugador) {
+
+        float velo = jugador.getLinearVelocity().len();
+        float variacionMin = 0.1f;
+        boolean minimoSuperado;
+
+        minimoSuperado = Math.abs(ultimaVelo - velo) >variacionMin;
+
+        if(minimoSuperado && ultimaVelo > velo){
+            this.zoom -= 0.02f;
+        } else if (minimoSuperado && velo > VELO_LIM_BAJO) {
+            this.zoom += 0.01f;
+        }
+
+        if (this.zoom < ZOOM_BAJO || velo < VELO_LIM_BAJO) {
+            this.zoom = ZOOM_BAJO;
+        } else if (this.zoom > ZOOM_ALTO || velo > VELO_LIM_ALTO) {
+            this.zoom = ZOOM_ALTO;
+        }
+        ultimaVelo = velo;
+    }
+
+}
