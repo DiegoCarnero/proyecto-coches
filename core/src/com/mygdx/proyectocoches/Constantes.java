14a5cede14c97256a27e3c43ad8793e822894118
Diego Carnero <diego.carnero.ortega@gmail.com>
2023-01-29 11:21:59 +0100

    Añadidas dos rutas extra

--- core/src/com/mygdx/proyectocoches/Constantes.java
+++ core/src/com/mygdx/proyectocoches/Constantes.java
@@ -14,7 +14,8 @@ public class Constantes {
     public static final String LAYER_CHECKP = "Checkpoint";
     public static final String LAYER_PATH = "Path";
 
-    public static final float MAX_VELOCIDAD_FORW = 13.0f;
+    public static final float MAX_VELO_IA = 24.0f;
+    public static final float MAX_VELOCIDAD_FORW = 9.0f;
     public static final float MAX_VELOCIDAD_BACK = 4.0f;
     public static final float DERRAPE_BAJO = 0.5f;
     public static final float DERRAPE_ALTO = 0.9f;
@@ -33,9 +34,15 @@ public class Constantes {
     public static final float SENSOR_SIZE = 80f;
 
     public static final String TEST_LOOP_NAME = "test_loop";
-    public static final Vector2[] test_loop_vGrid = {new Vector2(2,-5),new Vector2(3,-5.5f),new Vector2(4,-5),new Vector2(5,-5.5f),new Vector2(6,-5),new Vector2(7,-5.5f),new Vector2(8,-5),new Vector2(9,-5.5f)};
+    public static final Vector2[] test_loop_vGrid = {new Vector2(2,-5),new Vector2(2f,-5.5f),new Vector2(2,-6f),
+                                                    new Vector2(3,-5),new Vector2(3f,-5.5f),new Vector2(3,-6f),
+                                                    new Vector2(4,-5),new Vector2(4f,-5.5f),new Vector2(4,-6f),
+                                                    new Vector2(5,-5),new Vector2(5f,-5.5f),new Vector2(5,-6f),
+                                                    new Vector2(6,-5),new Vector2(6f,-5.5f),new Vector2(6,-6f),
+                                                    new Vector2(7,-5),new Vector2(7f,-5.5f),new Vector2(7,-6f),
+                                                    new Vector2(8,-5),new Vector2(8f,-5.5f),new Vector2(8,-6f)};
     public static final float test_loop_ang = -90;
-    public static final int TEST_LOOP_PATHS = 3;
+    public static final int TEST_LOOP_PATHS = 5;
 
     public static final int TELE_ACC = 1;
     public static final int TELE_EMBRAG = 2;
