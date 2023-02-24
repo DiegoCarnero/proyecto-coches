ed88996a41dd1e7829167ab2914d21722bcfd534
Diego Carnero <diegocarnero@users.noreply.github.com>
2023-02-24 23:05:33 +0100

    Añadida imagen de fondo

--- core/src/com/mygdx/proyectocoches/screens/MainMenu.java
+++ core/src/com/mygdx/proyectocoches/screens/MainMenu.java
@@ -37,7 +37,6 @@ import java.util.ArrayList;
 public class MainMenu implements Screen {
 
     private final ArrayList<Actor> compMain = new ArrayList<>();
-    private final ArrayList<Actor> compEvento = new ArrayList<>();
     private final Stage stage;
     private final InputMultiplexer multiplexer;
     private final AssetManager am;
@@ -47,9 +46,12 @@ public class MainMenu implements Screen {
     private final TutorialMenu mTutorial;
     private final CreditsScreen credits;
     private final SpriteBatch batch;
+    private final Sprite bg;
+    private final int screenW, screenH;
 
-    public MainMenu(final Game miGame, final Skin skin) {
-        int screenW, screenH;
+    public MainMenu(final Game miGame) {
+
+        Skin skin = new Skin(Gdx.files.internal("data/uiskin.json"));
 
         screenW = Gdx.graphics.getWidth();
         screenH = Gdx.graphics.getHeight();
@@ -60,6 +62,7 @@ public class MainMenu implements Screen {
         am.load("worlds/test_loop_mini.png", Texture.class);
         am.load("badlogic.jpg", Texture.class);
         am.load("locale/locale", I18NBundle.class);
+        am.load("data/carbon_fiber_bg.png",Texture.class);
 
         FileHandleResolver resolver = new InternalFileHandleResolver();
         am.setLoader(FreeTypeFontGenerator.class, new FreeTypeFontGeneratorLoader(resolver));
@@ -72,7 +75,7 @@ public class MainMenu implements Screen {
 
             FreetypeFontLoader.FreeTypeFontLoaderParameter param = new FreetypeFontLoader.FreeTypeFontLoaderParameter();
             param.fontFileName = fuentes[i];
-            param.fontParameters.characters = "\u0000AÁBCDEÉFGHIÍJKLMNÑOÓÖŐPQRSTUÚÜŰVWXYZaábcdeéfghiíjklmnñoóöőpqrstuúüűvwxyz1234567890\\\"!`?\\'.,;:()[]{}<>|/@\\\\^$\\u20ac-%+=º#_&~*\\u007f\\u0080\\u0081\\u0082\\u0083\\u0084\\u0085\\u0086\\u0087\\u0088\\u0089\\u008a\\u008b\\u008c\\u008d\\u008e\\u008f\\u0090\\u0091\\u0092\\u0093\\u0094\\u0095\\u0096\\u0097\\u0098\\u0099\\u009a\\u009b\\u009c\\u009d\\u009e\\u009f\\u00a0\\u00a1\\u00a2\\u00a3\\u00a4\\u00a5\\u00a6\\u00a7\\u00a8\\u00a9\\u00aa\\u00ab\\u00ac\\u00ad\\u00ae\\u00af\\u00b0\\u00b1\\u00b2\\u00b3\\u00b4\\u00b5\\u00b6\\u00b7\\u00b8\\u00b9\\u00ba\\u00bb\\u00bc\\u00bd\\u00be\\u00bf\\u00c0\\u00c1\\u00c2\\u00c3\\u00c4\\u00c5\\u00c6\\u00c7\\u00c8\\u00c9\\u00ca\\u00cb\\u00cc\\u00cd\\u00ce\\u00cf\\u00d0\\u00d1\\u00d2\\u00d3\\u00d4\\u00d5\\u00d6\\u00d7\\u00d8\\u00d9\\u00da\\u00db\\u00dc\\u00dd\\u00de\\u00df\\u00e0\\u00e1\\u00e2\\u00e3\\u00e4\\u00e5\\u00e6\\u00e7\\u00e8\\u00e9\\u00ea\\u00eb\\u00ec\\u00ed\\u00ee\\u00ef\\u00f0\\u00f1\\u00f2\\u00f3\\u00f4\\u00f5\\u00f6\\u00f7\\u00f8\\u00f9\\u00fa\\u00fb\\u00fc\\u00fd\\u00fe\\u00ff\"";
+            param.fontParameters.characters = "\u0000AÁBCDEÉFGHIÍJKLMNÑOÓÖŐPQRSTUÚÜŰVWXYZaábcdeéfghiíjklmnñoóöőpqrstuúüűvwxyz1234567890\\\"¡!`¿?\\'.,;:()[]{}<>|/@\\\\^$\\u20ac-%+=º#_&~*\\u007f\\u0080\\u0081\\u0082\\u0083\\u0084\\u0085\\u0086\\u0087\\u0088\\u0089\\u008a\\u008b\\u008c\\u008d\\u008e\\u008f\\u0090\\u0091\\u0092\\u0093\\u0094\\u0095\\u0096\\u0097\\u0098\\u0099\\u009a\\u009b\\u009c\\u009d\\u009e\\u009f\\u00a0\\u00a1\\u00a2\\u00a3\\u00a4\\u00a5\\u00a6\\u00a7\\u00a8\\u00a9\\u00aa\\u00ab\\u00ac\\u00ad\\u00ae\\u00af\\u00b0\\u00b1\\u00b2\\u00b3\\u00b4\\u00b5\\u00b6\\u00b7\\u00b8\\u00b9\\u00ba\\u00bb\\u00bc\\u00bd\\u00be\\u00bf\\u00c0\\u00c1\\u00c2\\u00c3\\u00c4\\u00c5\\u00c6\\u00c7\\u00c8\\u00c9\\u00ca\\u00cb\\u00cc\\u00cd\\u00ce\\u00cf\\u00d0\\u00d1\\u00d2\\u00d3\\u00d4\\u00d5\\u00d6\\u00d7\\u00d8\\u00d9\\u00da\\u00db\\u00dc\\u00dd\\u00de\\u00df\\u00e0\\u00e1\\u00e2\\u00e3\\u00e4\\u00e5\\u00e6\\u00e7\\u00e8\\u00e9\\u00ea\\u00eb\\u00ec\\u00ed\\u00ee\\u00ef\\u00f0\\u00f1\\u00f2\\u00f3\\u00f4\\u00f5\\u00f6\\u00f7\\u00f8\\u00f9\\u00fa\\u00fb\\u00fc\\u00fd\\u00fe\\u00ff\"";
             param.fontParameters.size = fontSizes[i];
             param.fontParameters.color = Color.WHITE;
             param.fontParameters.borderColor = Color.BLACK;
@@ -83,7 +86,7 @@ public class MainMenu implements Screen {
         }
 
         am.finishLoading();
-
+        bg = new Sprite((Texture) am.get("data/carbon_fiber_bg.png"));
         I18NBundle locale = am.get("locale/locale");
 
         stage = new Stage(new ScreenViewport());
@@ -261,17 +264,17 @@ public class MainMenu implements Screen {
     @Override
     public void render(float delta) {
         if (am.update()) {
+            batch.begin();
+            batch.draw(bg,0,0,screenW,screenH);
             if (mEvento.isShowing()) {
                 Sprite s = mEvento.getS();
-                batch.begin();
-                batch.draw(s, s.getX(), s.getY());
-                batch.end();
+                batch.draw(s, s.getX(), s.getY(),screenH / 7f, screenH / 7f);
             } else if (mTutorial.isShowing()) {
                 Sprite s = mTutorial.getS();
-                batch.begin();
-                batch.draw(s, s.getX(), s.getY());
-                batch.end();
+                batch.draw(s, s.getX(), s.getY(), screenH / 1.5f, screenH / 1.5f);
             }
+            batch.end();
+
             stage.act();
             stage.draw();
         }
